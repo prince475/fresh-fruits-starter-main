bed3c2fea8eb52ecf9fc7cfa78da1e6c49fe7e28
prince475 <danielmwai475@gmail.com>
2024-11-24 02:45:54 +0300

    Making adjustments and correction on the motion

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -7,9 +7,9 @@ const ResponsiveMenu = ({ open }) => {
     <AnimatePresence mode="wait">
       {open && (
           <motion.div
-          initial={{ opacity:0, y: -100 }}
-          animate={{opacity:1, y:0 }}
-          exit={{ opacity: 0, y:-100 }}
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -100 }}
           transition={{ duration: 0.3 }} 
           className="absolute top-20 left-0 w-full h-screen z-20"
           >
